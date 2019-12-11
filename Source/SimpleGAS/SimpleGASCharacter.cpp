bb5277594deddf71195caa771243a9bd50f6d519
substence <substence@gmail.com>
2019-12-11 06:18:03 -0800

    broken attribute on respawn

--- Source/SimpleGAS/SimpleGASCharacter.cpp
+++ Source/SimpleGAS/SimpleGASCharacter.cpp
@@ -117,10 +117,10 @@ void ASimpleGASCharacter::SetupAbilitySystemAndAttributes()
 		// Set the ASC on the Server. Clients do this in OnRep_PlayerState()
 		AbilitySystem = Cast<UAbilitySystemComponent>(PS->GetAbilitySystemComponent());
 
-		// AI won't have PlayerControllers so we can init again here just to be sure. No harm in initing twice for heroes that have PlayerControllers.
-		PS->GetAbilitySystemComponent()->InitAbilityActorInfo(PS, this);
-
 		AttributeSet = PS->AttributeSet;
+
+		// AI won't have PlayerControllers so we can init again here just to be sure. No harm in initing twice for heroes that have PlayerControllers.
+		AbilitySystem->InitAbilityActorInfo(PS, this);
 
 		if (!PS->bHasDefaultAbilities)
 		{
